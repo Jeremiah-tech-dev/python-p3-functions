03b40ff2d2b11806311b2fbda66470f4a14c575e
Jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2025-11-13 06:18:32 +0300

    Did the Python 3 lab test

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+def add(num1=45, num2=55):
+    print(num1 + num2)
+    add_result = num1 + num2
+    return add_result
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+    
